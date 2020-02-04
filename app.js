9c5e614d79e4b5863e473d3a4e88ec080faebb5e
soheima <soheima@sociossports.nl>
2020-02-04 21:24:21 +0100

    p5 custom brush for canvas details

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
 
-let flowers
-let hue
+let flowers = []
+ let hue
+let size 
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
-   flowers = []
-   hue = random(0,100)
  
 
 }
 
 function draw() {
- // background('#111118')
-
-flowers.forEach(flower => {
-  flower.draw()
-
-});
- 
-}
-
-function mouseDragged(){
-
-  hue = hue + 0.1
-
-  if (hue > 100)
-  {
-    hue = 0
+  //background("#111118")
+  
+  if (mouseIsPressed) {
+    let flower = new Flower(mouseX, mouseY, hue, size)
+    flowers.push(flower)
+    
+    hue += 0.5
+    size += 1
+    size = constrain(size, 20, 100)
+    
+    if (hue > 100) {
+      hue = 0
+    }
+    
+  } else {
+    hue = random(0, 100)
+    size = 20
   }
   
-  flowers.push(new Flower(mouseX, mouseY, hue))
+  flowers.forEach(flower => {
+    flower.draw()
+  })
 }
 
+function keyTyped() {
+  if (key === 's') {
+    saveCanvas("flowers", "png")
+  }
+}
 
-
-
-
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
--- flower.js
+++ flower.js
@@ -1,22 +1,51 @@
 class Flower {
-    constructor (x,y, hue){ 
+    constructor (x,y, hue, size){ 
         // what are things we need for our class
-        this.x = x
-        this.y = y
+        this.rotation = 0
+        this.rotationSpeed = random(-0.01, 0.01)
+        this.pos = createVector(x, y)
+        this.lines = random(7, 20)
+        this.midSize = size
+        this.petalSize = random(5, 15)
+        this.radius = (size / 2) + random(0, 20)
+        this.startRot = 0
+        this.rotSpeed = random(-0.0010, 0.010)
         this.hue = hue
-        this.midSize = random(20,40)
+      
+    
        
     }
     draw(){
         // a method on the flower class on how we draw the flower
-        colorMode(HSB,100)
+        this.startRot += this.rotSpeed
         noStroke()
-        fill(this.hue, 100,100)
+        colorMode(HSB,100)
+
+        fill(this.hue, 100,90)
     
          // we minus half of the square size from Y and X
         // square(mouseX-30,mouseY -30, 60)
         //square(this.x-25, this.y-25, 50)
         //fill(this.hueMini,100,100)
-        circle(this.x, this.y, this.midSize)
+        circle(this.pos.x, this.pos.y, this.midSize)
+
+
+        for (let num = 0; num <  this.lines; num = num + 1) {
+
+            let angle = TWO_PI * num / this.lines + this.startRot
+            // let px =  this.petalDist  * cos(angle)
+            // let py =  this.petalDist * sin(angle)
+
+            // branch does the cos and sin 
+            let branch = createVector(this.radius, 0)
+            branch.rotate(angle)
+            branch.add(this.pos)
+
+
+            circle(branch.x, branch.y, this.petalSize)
+
+        }
+
     }
 }
+
